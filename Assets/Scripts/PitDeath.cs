ee340fecc59347783399576e3a2bfa432bdb7e64
sustachio <sustachio@gmail.com>
2023-06-24 12:35:11 -0500

    Fix: fix player death

--- Assets/Scripts/PitDeath.cs
+++ Assets/Scripts/PitDeath.cs
@@ -9,23 +9,14 @@ public class PitDeath : MonoBehaviour
 
     Player playerScript;
 
-    // Start is called before the first frame update
     void Start()
     {
         playerScript = player.GetComponent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
+        if (collision.gameObject == player)
             playerScript.Death();
-        }
     }
 }
