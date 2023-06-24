bb64b1bd5a9a695332533447dc835a6b86515c01
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-23 19:34:53 -0500

    Feature:Player Movement Beta

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalMove = 0f;
+
+    float runSpeed = 15f;
+    bool jump = false;
+
+    [SerializeField] private float jumpForce = 400f;                          // Amount of force added when the player jumps.
+    [SerializeField] private LayerMask whatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform groundCheck;                           // A position marking where to check if the player is grounded.
+
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool grounded;            // Whether or not the player is grounded.
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime;
+
+        transform.Translate(horizontalMove, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        Move(jump);
+        jump = false;
+
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+            }
+        }
+    }
+
+    void Move(bool jump)
+    {
+        // If the player should jump...
+        if (grounded && jump)
+        {
+            // Add a vertical force to the player.
+            grounded = false;
+            rb2D.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+}
